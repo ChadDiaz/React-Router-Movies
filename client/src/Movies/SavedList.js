c0b91919cad0b28da0e580f92791ae440f6a4676
ChadDiaz <chadcdiaz@gmail.com>
2020-09-30 20:57:16 -0400

    changed where the useHistory hook was being used. MVP

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,21 +1,20 @@
 import React from 'react';
-import { Link } from 'react-router-dom'
+import { Link, useHistory } from 'react-router-dom'
 
 
 export default function SavedList(props) {
-  console.log("savedList Props" , props)
+  // console.log("savedList Props" , props)
+  const history = useHistory()
   const routeToHome = () => {
-    props.history.push("/")
+    history.push("/")
   }
   return (
     <div className="saved-list">
       <h3>Saved Movies:</h3>
       {props.list.map(movie => (
         <span className="saved-movie">{movie.title}</span>
-      ))}
-      
+      ))}      
         <button className="home-button" onClick={() => routeToHome()} >Home</button>
-      
     </div>
   );
 }
